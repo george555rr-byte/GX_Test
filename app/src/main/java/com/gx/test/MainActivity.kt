0158b98f3be00fe49b8d3db49d13f483bde5e798
george555rr-byte <george555rr@gmail.com>
2026-09-16 14:34:44 +0300

    Add MainActivity with basic TextView display

---
+++ app/src/main/java/com/gx/test/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.gx.test
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tv = TextView(this)
+        tv.text = "GX Test Works!"
+        setContentView(tv)
+    }
+}
